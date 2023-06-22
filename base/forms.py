49c471a064352a51d3778d94fe5e094136ae0503
maruf128 <maruf@live.nl>
2023-06-22 11:56:09 +0200

    ervoor gezorgd dat alles alvast bestaat nu copy

---
+++ base/forms.py
@@ -0,0 +1,14 @@
+# from django import forms
+# from .models import Distance, Time
+
+# class NameForm(forms.Form):
+#     your_name = forms.CharField(label="Your name", max_length=100)
+#     your_surname = forms.CharField(label="Your surname", max_length=100)
+
+
+# class DistanceForm(forms.ModelForm):
+#     class Meta:
+#         model = Distance
+#         fields = ("length", "full_name")
+
+# voorbeelden ^^
--- base/views.py
+++ base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# from .models import Distance, Time
+# from .forms import NameForm, DistanceForm, TimeForm
+# from django.shortcuts import redirect
+# from django.contrib import messages
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth import login
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.admin.views.decorators import staff_member_required
+
 
 # Create your views here.
+def index(request):
+    return render(request, "base/index.html")
