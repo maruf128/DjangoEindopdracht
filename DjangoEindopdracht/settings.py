1ba443ab57823d44a4bece91caca4a9ec5a1220c
maruf128 <maruf@live.nl>
2023-06-24 15:23:04 +0200

    registratie, login en logout werkende

--- DjangoEindopdracht/settings.py
+++ DjangoEindopdracht/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
